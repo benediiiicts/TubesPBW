a825f4ac0006706867fdbd547fe1ba63e3d1068b
BENEDICTUS KRISTIADI A W <benedictuskristiadi@gmail.com>
2024-12-12 07:47:47 +0700

    edit add-artist

--- pbw/pbw/src/test/java/com/tubes/pbw/PbwApplicationTests.java
+++ pbw/pbw/src/test/java/com/tubes/pbw/PbwApplicationTests.java
@@ -1,6 +1,8 @@
 package com.tubes.pbw;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
