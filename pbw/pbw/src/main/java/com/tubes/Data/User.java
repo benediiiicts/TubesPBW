918451e06de5175646238a43d3f403c0682bc040
ZEFANDION BENAYA TEJA <i22042@ftis>
2024-12-12 11:26:21 +0700

    Login sudah connect

--- pbw/pbw/src/main/java/com/tubes/Data/User.java
+++ pbw/pbw/src/main/java/com/tubes/Data/User.java
@@ -1,8 +1,27 @@
 package com.tubes.Data;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class User {
-    
+    @NotNull
+    private String email;
+
+    @NotNull
+    @Size(min=4, max=50)
+    private String username;
+
+    @NotNull
+    @Size(min=4, max=60)
+    private String password;
+
+    @NotNull
+    @Size(min=4, max=60)
+    private String confirmpassword;
+
+    private String role = "member";
 }
--- pbw/pbw/src/main/java/com/tubes/pbw/Login/LoginController.java
+++ pbw/pbw/src/main/java/com/tubes/pbw/Login/LoginController.java
@@ -1,12 +1,88 @@
 package com.tubes.pbw.Login;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.tubes.Data.User;
+import com.tubes.pbw.User.JdbcUserRepository;
+import com.tubes.pbw.User.UserService;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 public class LoginController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JdbcUserRepository jdbcUserRepository;
+    
     @GetMapping("/login")
-    public String redirectToLogin() {
+    public String redirectToLogin(HttpSession session) {
+        // Jika user sudah login, redirect ke dashboard
+        if (session.getAttribute("loggedUser") != null) {
+            return "redirect:/home";
+        }
         return "login";
     }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("email") String email,
+                        @RequestParam("password") String password,
+                        Model model,
+                        HttpSession session) {
+        // Panggil fungsi login di UserService
+        User user = userService.login(email, password);
+        if (user != null) {
+            // Jika user valid, simpan ke session dan redirect ke dashboard
+            session.setAttribute("loggedUser", user);
+            return "redirect:/home";
+        } else {
+            // Jika user tidak ditemukan atau password salah, tampilkan error
+            model.addAttribute("status", "failed");
+            return "login";
+        }
+    }
+
+    @GetMapping("/register")
+    public String registerView(User user){
+        return "login";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid User user, BindingResult bindingResult ){
+        if(!user.getPassword().equals(user.getConfirmpassword())){
+            bindingResult.rejectValue("confirmpassword", "passwordMissmatch", "password do not match");
+        }
+
+        if(!userService.register(user)){
+            bindingResult.rejectValue("username", "usernameAlreadyExists", "username already exists" );
+        }
+       
+        if(bindingResult.hasErrors()){
+            return "/login";
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        try {
+            jdbcUserRepository.save(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/";
+    }
+
+    
+
 }
