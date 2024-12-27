4f6e84129cf7da7c3f9585b1a2bd2e3d934fea1f
BENEDICTUS KRISTIADI A W <benedictuskristiadi@gmail.com>
2024-12-27 23:03:49 +0700

    update page artists, artost-detail, aspect java, req role

--- pbw/pbw/src/main/java/com/tubes/pbw/Artist/ArtistsController.java
+++ pbw/pbw/src/main/java/com/tubes/pbw/Artist/ArtistsController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,13 +16,26 @@ public class ArtistsController {
     @Autowired
     ArtistsService artistsService;
 
-    @GetMapping("/artists")
-    public String redirectToArtists() {
-        return "artists";
-    }
     @GetMapping("/add-artist")
     public String redirectToAddArtists() {
         return "add-artist";
     }
 
+    @GetMapping("/artists")
+    public String showArtists(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size,
+            Model model) {
+        List<Artist> artists = artistsService.getArtistsByPage(page, size);
+        int totalPages = artistsService.getTotalPages(size);
+        
+        model.addAttribute("artists", artists);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", totalPages);
+        return "artists";
+    }
+    @GetMapping("/artist-detail")
+    public String redirectToArtistDetail(){
+        return "artist-detail";
+    }
 }
