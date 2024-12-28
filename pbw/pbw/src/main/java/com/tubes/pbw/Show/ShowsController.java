7649c69426722b1e96b67dc868b1bfb4fd8015a5
Alvin <192010003@smatalenta.sch.id>
2024-12-28 18:51:32 +0700

    Penambahan Profile Login

--- pbw/pbw/src/main/java/com/tubes/pbw/Show/ShowsController.java
+++ pbw/pbw/src/main/java/com/tubes/pbw/Show/ShowsController.java
@@ -3,12 +3,23 @@ package com.tubes.pbw.Show;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpSession;
+import com.tubes.Data.User;
+import org.springframework.ui.Model;
+
 @Controller
 public class ShowsController {
     @GetMapping("/shows")
-    public String redirectToShows() {
-        return "shows";
+    public String shows(HttpSession session, Model model) {
+        // Ambil user dari session
+        User loggedUser = (User) session.getAttribute("loggedUser");
+        if (loggedUser != null) {
+            // Jika user sudah login, tambahkan nama user ke model
+            model.addAttribute("user", loggedUser);
+        }
+        return "shows"; // Mengarahkan ke halaman shows
     }
+
     @GetMapping("/add-show")
     public String redirectToAddShow() {
         return "add-show";
