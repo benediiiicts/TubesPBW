2ad392f92dca29aedb1c032181574f607f8482ff
Alvin <192010003@smatalenta.sch.id>
2024-12-30 19:49:24 +0700

    Update

--- pbw/pbw/src/main/resources/static/js/home.js
+++ pbw/pbw/src/main/resources/static/js/home.js
@@ -4,39 +4,33 @@ function toggleMenu() {
     sidebar.classList.toggle('active');
 }
 
-//slideshow
-let slideIndex = new Map();
-function showSlides(slide_id) {
-    let slides = document.querySelectorAll(`#${slide_id} .slides`);
-    if(!slideIndex.has(slide_id)){
-        slideIndex.set(slide_id, 0);
-    }
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-        slides[i].style.opacity = "0";
-        slides[i].getElementsByClassName("caption")[0].style.opacity = "0"; 
-    }
+//slide show
+let slideIndex = 0;
 
-    let index = slideIndex.get(slide_id);
-
-    slides[index].style.display = "block";  
-    setTimeout(() => {
-        slides[index].style.opacity = "1";
-        slides[index].getElementsByClassName("caption")[0].style.opacity = "1";
-    }, 10);
+function plusSlides(n, slideshowId) {
+    const slides = document.querySelectorAll(`#${slideshowId} .slides`);
+    slideIndex += n;
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+    slides.forEach((slide) => {
+        slide.style.display = "none";
+    });
+    slides[slideIndex].style.display = "block";
 }
-function plusSlides(n, slide_id) {
-    let slides = document.querySelectorAll(`#${slide_id} .slides`);
-    if(!slideIndex.has(slide_id)){
-        slideIndex.set(slide_id, 0);
-    }
-    let index = slideIndex.get(slide_id);
-    index += n;
-    if(index < 0) index = slides.length-1;
-    if (index >= slides.length) { index = 0; }
 
-    slideIndex.set(slide_id, index);
-    showSlides(slide_id);
+function showSlides(slideshowId) {
+    const slides = document.querySelectorAll(`#${slideshowId} .slides`);
+    const delay = 5000; // Waktu tunggu antara slide (dalam milidetik)
+
+    slides[0].style.display = "block";
+
+    setInterval(() => {
+        plusSlides(1, slideshowId);
+    }, delay);
 }
+
 showSlides('slide-rec');
 showSlides('slide-otp');
