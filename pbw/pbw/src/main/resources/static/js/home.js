afa24ce629b55d61969c870fbcc28d01a08afa9f
BENEDICTUS KRISTIADI A W <benedictuskristiadi@gmail.com>
2024-12-30 21:55:50 +0700

    perbaiki slideshow home

--- pbw/pbw/src/main/resources/static/js/home.js
+++ pbw/pbw/src/main/resources/static/js/home.js
@@ -1,36 +1,60 @@
-//sidebar
+// Sidebar
 function toggleMenu() {
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('active');
 }
 
-//slide show
-let slideIndex = 0;
-
-function plusSlides(n, slideshowId) {
-    const slides = document.querySelectorAll(`#${slideshowId} .slides`);
-    slideIndex += n;
-    if (slideIndex >= slides.length) {
-        slideIndex = 0;
-    } else if (slideIndex < 0) {
-        slideIndex = slides.length - 1;
+// Slideshow
+let slideIndex = new Map();
+function showSlides(slide_id) {
+    let slides = document.querySelectorAll(`#${slide_id} .slides`);
+    if (!slideIndex.has(slide_id)) {
+        slideIndex.set(slide_id, 0);
     }
-    slides.forEach((slide) => {
-        slide.style.display = "none";
-    });
-    slides[slideIndex].style.display = "block";
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+        slides[i].style.opacity = "0";
+        slides[i].getElementsByClassName("caption")[0].style.opacity = "0"; 
+    }
+
+    let index = slideIndex.get(slide_id);
+
+    slides[index].style.display = "block";  
+    setTimeout(() => {
+        slides[index].style.opacity = "1";
+        slides[index].getElementsByClassName("caption")[0].style.opacity = "1";
+    }, 10);
 }
 
-function showSlides(slideshowId) {
-    const slides = document.querySelectorAll(`#${slideshowId} .slides`);
-    const delay = 5000; // Waktu tunggu antara slide (dalam milidetik)
+let slideIntervals = new Map();
 
-    slides[0].style.display = "block";
+function resetAutoSlide(slide_id, interval) {
+    if (slideIntervals.has(slide_id)) {
+        clearInterval(slideIntervals.get(slide_id));
+    }
+    const newInterval = setInterval(() => {
+        plusSlides(1, slide_id);
+    }, interval);
+    slideIntervals.set(slide_id, newInterval);
+}
 
-    setInterval(() => {
-        plusSlides(1, slideshowId);
-    }, delay);
+function plusSlides(n, slide_id) {
+    let slides = document.querySelectorAll(`#${slide_id} .slides`);
+    if (!slideIndex.has(slide_id)) {
+        slideIndex.set(slide_id, 0);
+    }
+    let index = slideIndex.get(slide_id);
+    index += n;
+    if (index < 0) index = slides.length - 1;
+    if (index >= slides.length) { index = 0; }
+
+    slideIndex.set(slide_id, index);
+    showSlides(slide_id);
+    resetAutoSlide(slide_id, 3000);
 }
 
 showSlides('slide-rec');
 showSlides('slide-otp');
+
+resetAutoSlide('slide-rec', 3000);
+resetAutoSlide('slide-otp', 3000);
