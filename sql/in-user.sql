a99557906c02a9e3c1d86ba830b42b1be6960da6
BENEDICTUS KRISTIADI A W <benedictuskristiadi@gmail.com>
2025-01-04 22:59:00 +0700

    query insert admin, update artis-detail, show, setlist, song

---
+++ sql/in-user.sql
@@ -0,0 +1,3 @@
+--input user admin awal
+insert into users (email, username, password, role)
+values ('admin@gmail.com', 'admin', '$2a$10$OckDVVGZrVxDND9dH04lGeJ50HRJfIqYsk/wYkxYxBDpyZwRpgL5q', 'admin')
