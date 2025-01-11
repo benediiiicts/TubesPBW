67e4134b0d16001e334bf8524b706b6af7bd4f7a
BENEDICTUS KRISTIADI A W <benedictuskristiadi@gmail.com>
2025-01-11 15:40:51 +0700

    update sql comment

--- sql/cr-comment.sql
+++ sql/cr-comment.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS comment CASCADE;
 CREATE TABLE comment (
     id SERIAL PRIMARY KEY,
     commentar TEXT,
+	date date,
     id_setlist INT NOT NULL,
     id_user INT NOT NULL,
     FOREIGN KEY (id_user) REFERENCES users(id),
